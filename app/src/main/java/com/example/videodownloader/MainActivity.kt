e8f4c340a5bb3aea9c259ece4ce5e44062521843
21040897 <gkals0105@gmail.com>
2026-08-18 00:32:23 +0900

    Create MainActivity.kt

---
+++ app/src/main/java/com/example/videodownloader/MainActivity.kt
@@ -0,0 +1,32 @@
+@file:OptIn(androidx.compose.material3.ExperimentalMaterial3Api::class)
+
+package com.example.videodownloader
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Box(contentAlignment = Alignment.Center) {
+                        Text("앱이 성공적으로 만들어졌습니다!")
+                    }
+                }
+            }
+        }
+    }
+}
